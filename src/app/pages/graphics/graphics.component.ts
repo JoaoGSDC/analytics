31cdf963038622958bd6a18ad075238297e078ac
JoaoGSDC <joaogsdc@gmail.com>
2020-04-10 18:37:33 -0300

    Graficos

--- src/app/pages/graphics/graphics.component.ts
+++ src/app/pages/graphics/graphics.component.ts
@@ -8,12 +8,135 @@ import { Chart } from 'chart.js';
 })
 export class GraphicsComponent implements OnInit {
 
+  graphicFooter: string[] = ["Books and magazines", "Clothing and clothing accessories", "Computer hardware", "Computer software", "Drugs, health aids, and beauty aids", "Food, beer, and wine"]
+
   LineChart = [];
+  BarChart = [];
+  PieChart = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    
+    this.createBarsGraphic();
+    this.createLineGraphic();
+    this.createPieGraphic();
+  }
+
+  createBarsGraphic(): void {
+    this.BarChart = new Chart('barChart', {
+      type: 'bar',
+      data: {
+        labels: this.graphicFooter,
+        datasets: [{
+          label: '# of Votes',
+          data: [1999, 2000, 2001, 2002, 2003, 2004],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        title: {
+          text: "Gráfico de Barras",
+          display: true
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: false
+            }
+          }]
+        }
+      }
+    });
+  }
+
+  createLineGraphic(): void {
+    this.LineChart = new Chart('lineChart', {
+      type: 'line',
+      data: {
+        labels: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        datasets: [{
+          label: 'Number of Items Sold in Months',
+          data: [9, 7, 3, 5, 2, 10, 15, 16, 19, 3, 1, 9],
+          fill: false,
+          lineTension: 0.2,
+          borderColor: "red",
+          borderWidth: 1
+        }]
+      },
+      options: {
+        title: {
+          text: "Line Chart",
+          display: true
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
+  createPieGraphic(): void {
+    this.PieChart = new Chart('pieChart', {
+      type: 'pie',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [9, 7, 3, 5, 2, 10],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        title: {
+          text: "Bar Chart",
+          display: true
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+
   }
 
 }
